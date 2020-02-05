37ca3e8e97619335557481ba5984cca5f8853147
Duncan Wright <1002243w@student.gla.ac.uk>
2020-02-05 15:17:23 +0000

    Chapter 6 completed - category list on home page

--- rango/urls.py
+++ rango/urls.py
@@ -6,4 +6,6 @@ app_name = 'rango'
 urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
+    #use of category_name_slug below must match parameter name in view definition
+    path('category/<slug:category_name_slug>/', views.show_category, name='show_category'),
 ]
--- rango/views.py
+++ rango/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rango.models import Category, Page
 
 # Create your views here.
 def index(request):
-    # constructu a dictionary to pass template engine as its context
+    # construct a dictionary to pass template engine as its context
     # boldmessage matches template variable in index.html
-    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    # query database for all categories, order by number of likes
+    # retrieve only top 5, place in context_dict
+    category_list = Category.objects.order_by('-likes')[:5]
+    page_list = Page.objects.order_by('-views')[:5]
+
+    context_dict = {}
+    context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
+    context_dict['categories'] = category_list
+    context_dict['pages'] = page_list
 
     # return rendered response to send to the client
     return render(request, 'rango/index.html', context=context_dict)
@@ -13,3 +22,33 @@ def index(request):
 
 def about(request):
     return render(request, 'rango/about.html', {})
+
+def show_category(request, category_name_slug):
+    context_dict = {}
+
+    try:
+        # find category from slug?
+        category = Category.objects.get(slug=category_name_slug)
+
+        # retrieve all pages in this category (using filter())
+        pages = Page.objects.filter(category=category)
+
+        # Add results to context dict
+        context_dict['pages'] = pages
+
+        # Also add category to verify (in the template) it exists
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+        context_dict['category'] = None
+        context_dict['pages'] = None
+
+    return render(request, 'rango/category.html', context=context_dict)
+
+
+
+
+
+
+
+
+
